a5db072af7c40a8283232f3914296b5f73609d51
Alvin Leonardo <44573205+alvinvin00@users.noreply.github.com>
2019-05-18 21:58:49 +0700

    Changed Date Format string

--- lib/utilities/utilities.dart
+++ lib/utilities/utilities.dart
@@ -3,27 +3,28 @@ import 'package:intl/intl.dart';
 class Utilities {
   String convertTimestamp(int timestamp) {
     var now = new DateTime.now();
-    var format = new DateFormat('HH:mm');
-    var date = new DateTime.fromMicrosecondsSinceEpoch(timestamp * 1000);
+    var dateFormat = new DateFormat('dd/MM/yy');
+    var timeFormat = new DateFormat('HH:mm');
+    var date = new DateTime.fromMillisecondsSinceEpoch(timestamp);
     var diff = now.difference(date);
-    var timeText = '';
+    var text = '';
 
     if (diff.inSeconds <= 0 ||
         diff.inSeconds > 0 && diff.inMinutes == 0 ||
         diff.inMinutes > 0 && diff.inHours == 0 ||
         diff.inHours > 0 && diff.inDays == 0) {
-      timeText = format.format(date);
+      text = timeFormat.format(date);
     } else {
-      timeText = diff.inDays.toString() + ' HARI YANG LALU';
+      text = dateFormat.format(date);
     }
-    return timeText;
+    return text;
   }
 
   String convertCommentTimestamp(int timestamp) {
     var now = new DateTime.now();
     var timeFormat = new DateFormat('HH:mm');
-    var dateFormat = new DateFormat('D/M');
-    var date = new DateTime.fromMicrosecondsSinceEpoch(timestamp * 1000);
+    var dateFormat = new DateFormat('d/M');
+    var date = new DateTime.fromMillisecondsSinceEpoch(timestamp);
     var diff = now.difference(date);
     var text = '';
 
